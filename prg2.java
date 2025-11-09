b0f5d43a83bcd3a2fcf3f9453241d3a7bd1f57ba
Nirmal <123132335+Nirmal1326@users.noreply.github.com>
2025-11-09 18:56:27 +0530

    Add files via upload

---
+++ prg2.java
@@ -0,0 +1,8 @@
+public class prg2{
+public static void main(String[] args) {
+System.out.println("Welcome");
+System.out.println("To");
+System.out.println("DevOps");
+System.out.println("Class");
+}
+}
